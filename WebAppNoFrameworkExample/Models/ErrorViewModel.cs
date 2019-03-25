fa2a1c5673e7978eeff97053f4df2891c75ae103
Brian Mills <brian@millsit.com>
2019-03-25 13:57:29 +0800

    Add project files.

---
+++ WebAppNoFrameworkExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppNoFrameworkExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
